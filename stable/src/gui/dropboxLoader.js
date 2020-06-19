b91d49ccb3c42dca5d6f9680cf530fc516d37a63
andre23arruda <andre23arruda@gmail.com>
2020-06-19 12:24:21 -0300

    feat: Add icon to redirect to harpiahealth.com

--- stable/src/gui/dropboxLoader.js
+++ stable/src/gui/dropboxLoader.js
@@ -117,7 +117,83 @@ dwvjq.gui.DropboxLoader = function (app)
         event.stopPropagation();
         event.preventDefault();
         // load files
-        app.loadFiles(event.dataTransfer.files);
+
+        if (event.dataTransfer.files.length > 1) {
+
+            app['xurlfiles'] = []
+
+            // arquivos carregados
+            var files_img = (Array.from(event.dataTransfer.files)).filter(value => value.name.split(".").pop().toLowerCase() !== "json")
+            var files_json = (Array.from(event.dataTransfer.files)).filter(value => value.name.split(".").pop().toLowerCase() === "json")
+
+            if (files_img.length){
+                app.xfiles = files_img
+                app.xjson = files_json
+                app.xcurrentSlice = 1
+            }
+
+            var file_img = files_img[0].name.split(".").shift()
+            var file_json = (files_json).filter(value => value.name.includes(file_img))
+            app.loadFiles([app.xfiles[app.xcurrentSlice - 1]]);
+            if (file_json.length) {
+                setTimeout(function(){ console.log('teste')
+                app.loadFiles([file_json[0]])}, 500);
+            }
+
+            // >>
+            var fileNext = document.querySelector("#next_image");
+            fileNext.onclick = function(){
+                app.xcurrentSlice += 1
+                if (app.xcurrentSlice > app.xfiles.length){
+                    app.xcurrentSlice = app.xfiles.length
+                }
+                else{
+                    file_img = app.xfiles[app.xcurrentSlice - 1].name.split(".").shift()
+                    file_json = (files_json).filter(value => value.name.includes(file_img))
+                    app.loadFiles([app.xfiles[app.xcurrentSlice - 1]]);
+                    if (file_json.length) {
+                        setTimeout(function(){ console.log('teste')
+                        app.loadFiles([file_json[0]])}, 500);
+                    }
+                }
+            }
+
+            // <<
+            var fileBefore = document.querySelector("#previous_image");
+            fileBefore.onclick = function(){
+                app.xcurrentSlice -= 1
+                if (app.xcurrentSlice < 1){
+                    app.xcurrentSlice = 1
+                }
+                else {
+                    file_img = app.xfiles[app.xcurrentSlice - 1].name.split(".").shift()
+                    file_json = (files_json).filter(value => value.name.includes(file_img))
+                    app.loadFiles([app.xfiles[app.xcurrentSlice - 1]]);
+                    if (file_json.length) {
+                        setTimeout(function(){ console.log('teste')
+                        app.loadFiles([file_json[0]])}, 500);
+                    }
+                }
+            }
+
+            // keyboard shortcuts listener
+            var keyShortcut = function (event) {
+                if (event.keyCode === 39 ) { // key -> next image
+                    fileNext.click()
+                }
+                else if (event.keyCode === 37 ) { // key <- previous image
+                    fileBefore.click()
+                }
+            }
+            window.addEventListener("keydown", keyShortcut);
+        }
+        else {
+            app.loadFiles(event.dataTransfer.files)
+        }
+
+
+
+        //app.loadFiles(event.dataTransfer.files);
         // hide drop box
         self.hideDropboxElement();
     }
